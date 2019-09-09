8d8218286c3019d3771387d1af1144f166551d9f
Daniel Caldera <bdcaldera@gmail.com>
2019-09-09 02:10:49 -0500

    Se logro la primera aplicacion en donde se muestran tres imagenes utilizando http request

---
+++ platzigram/posts/apps.py
@@ -0,0 +1,9 @@
+"""Posts application module."""
+
+from django.apps import AppConfig
+
+
+class PostsConfig(AppConfig):
+    """Pst application settings."""
+    name = 'posts'
+    varbose_name = 'Posts'
