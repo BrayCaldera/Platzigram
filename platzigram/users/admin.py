49bff421de3a0e6562a02f91825270b00a523962
Daniel Caldera <bdcaldera@gmail.com>
2019-09-14 00:19:43 -0500

    #007 (rm image)

--- platzigram/users/admin.py
+++ platzigram/users/admin.py
@@ -3,7 +3,28 @@
 # Django
 from django.contrib import admin
 
+# Models
 from users.models import Profile
 
-# Register your models here.
-admin.site.register(Profile)
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    """Profile Admin."""
+
+    list_display = ('pk', 'user', 'phone_number', 'website', 'picture')
+    list_display_links = ('pk', 'user')
+    list_editable = ('website', 'picture', 'phone_number')
+
+    search_fields = (
+        'user__email',
+        'user__username',
+        'user__first_name',
+        'user__last_name',
+        'phone_number'
+    )
+
+    list_filter = (
+        'user__is_active',
+        'user__is_staff',
+        'created',
+        'modified',
+    )
