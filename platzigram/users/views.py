ac5b0b58221ae9b5b9689285e51612731850ee64
Daniel Caldera <bdcaldera@gmail.com>
2019-09-14 18:03:11 -0500

    #013 (middleware)

--- platzigram/users/views.py
+++ platzigram/users/views.py
@@ -15,7 +15,17 @@ from users.models import Profile
 
 def update_profile(request):
     """Update usr's profile view"""
-    return render(request, 'users/update_profile.html')
+    profile = request.user.profile
+
+    return render(
+        request=request,
+        template_name='users/update_profile.html',
+        context={
+            'profile': profile,
+            'user': request.user
+        }
+        
+    )
 
 def login_view(request):
     """Login view."""
