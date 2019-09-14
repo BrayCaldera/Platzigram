a0977a5e3a1d395f9b752fa202ce6824b3605060
Daniel Caldera <bdcaldera@gmail.com>
2019-09-14 01:28:14 -0500

    #008

--- platzigram/platzigram/urls.py
+++ platzigram/platzigram/urls.py
@@ -18,6 +18,8 @@ Including another URLconf
 
 # Django
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 # Utilities
@@ -33,4 +35,4 @@ urlpatterns = [
     path('hi/<str:name>/<int:age>', local_views.say_hi),
 
     path('posts/', posts_views.list_posts),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
